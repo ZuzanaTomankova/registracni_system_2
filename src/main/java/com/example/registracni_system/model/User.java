eaad871292cd92566f4830ab2ffd9127ee423891
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-05-15 00:54:48 +0200

    vytvoření konstruktorů

--- src/main/java/com/example/registracni_system/model/User.java
+++ src/main/java/com/example/registracni_system/model/User.java
@@ -19,6 +19,13 @@ public class User {
     }
 
     public User(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+    public User( String name, String surname) {
+        this.name = name;
+        this.surname = surname;
     }
 
     public int getId() {
