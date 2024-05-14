cfdaaa2096e238a36ccf17a5005b370eff494870
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-05-15 00:55:19 +0200

    vytvoření kontrolerů

--- src/main/java/com/example/registracni_system/controller/RegistrationController.java
+++ src/main/java/com/example/registracni_system/controller/RegistrationController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 public class RegistrationController {
@@ -17,7 +18,34 @@ public class RegistrationController {
         registrationService.createUser(user);
     }
 
+    @GetMapping("api/v1/user/{id}")
+    public User getUserById(@PathVariable("id") int id) throws SQLException {
+        return registrationService.getUserById(id);
 
+    }
 
+    @GetMapping("api/v1/user/{id}/detail")
+    public User getUserByIdWithDetail(@PathVariable("id") int id) throws SQLException {
+        return registrationService.getUserByIdWithDetail(id);
 
+    }
+
+    @GetMapping("api/v1/users")
+    public List<User> getAllUsers() throws SQLException {
+        return registrationService.getAllUsers();
+    }
+
+    @GetMapping("api/v1/users/detail")
+    public List<User> getAllUsersWithDetails() throws SQLException {
+        return registrationService.getAllUsersWithDetails();
+    }
+
+    @PutMapping("api/v1/user/{id}")
+    public void updateUser(@RequestBody User user,@PathVariable ("id") int id) throws SQLException {
+        registrationService.updateUser(user);}
+
+    @DeleteMapping("api/v1/user/{id}")
+    public void deleteUser(@PathVariable("id") int id)throws SQLException {
+        registrationService.deleteUser(id);
+    }
 }
